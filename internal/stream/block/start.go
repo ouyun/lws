c2a3d853644b551d3bdc95db22254ea6da2370bc
soooooot <sooooootZHOU@gmail.com>
2018-09-10 11:46:04 +0800

    REFRACTOR(block): using pubsub module

--- internal/stream/block/start.go
+++ internal/stream/block/start.go
@@ -2,12 +2,17 @@ package block
 
 import (
 	"context"
+
 	"github.com/lomocoin/lws/internal/coreclient"
+	"github.com/lomocoin/lws/internal/stream/pubsub"
 )
 
 func Start(ctx context.Context, cclient *coreclient.Client) {
-	go subscribe(ctx, cclient)
-	go listenConsumer(ctx)
+	subscriber := NewSubscribe(ctx, cclient)
+	consumer := NewBlockConsumer()
+
+	go pubsub.ListenConsumer(ctx, consumer)
+	go subscriber.Subscribe()
 
 	<-ctx.Done()
 }
