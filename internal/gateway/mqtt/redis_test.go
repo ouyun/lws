c2d9ba9884d87b79090c47bdaef79a3d60b58574
dao <dao@daodeMacBook-Pro.local>
2018-09-25 15:59:09 +0800

    CHANGE:增加 redis test

--- internal/gateway/mqtt/redis_test.go
+++ internal/gateway/mqtt/redis_test.go
@@ -1,9 +1,13 @@
 package mqtt
 
 import (
+	"encoding/hex"
 	// "log"
+	"reflect"
+	"strconv"
 	"testing"
-	// "github.com/gomodule/redigo/redis"
+
+	"github.com/gomodule/redigo/redis"
 )
 
 func TestNewRedisPool(t *testing.T) {
@@ -14,6 +18,47 @@ func TestNewRedisPool(t *testing.T) {
 		t.Error("conn redis fail")
 	}
 	defer red.Close()
+}
+
+func TestRedis(t *testing.T) {
+	pool := GetRedisPool()
+	red := pool.Get()
+	err := red.Err()
+	if err != nil {
+		t.Error("conn redis fail")
+	}
+	defer red.Close()
+	cli := CliMap{}
+	address, _ := hex.DecodeString("7f937c2f5944f5da2a118cebb067cd2c9c92c75955ce05aa05158a1af28e1607")
+
+	cli.Address = address
+	cli.AddressId = uint32(2323)
+	cli.ApiKey = address
+	cli.ReplyUTXON = uint16(160)
+	cli.TopicPrefix = "wawawawaw"
+	// _, err = red.Do("SET", "jojo", "COCO")
+	// _, err = red.Do("HMSET", redis.Args{}.Add("tsau").AddFlat(cli)...)
+	err = SaveToRedis(&red, &cli)
+	if err != nil {
+		t.Error("save to redis fail")
+	}
+	cliMap := CliMap{}
+	value, err := redis.Values(red.Do("hgetall", strconv.FormatUint(uint64(cli.AddressId), 10)))
+	// log.Printf("value %+v! \n", value)
+	redis.ScanStruct(value, &cliMap)
+	if err != nil {
+		t.Error("get redis failed")
+	}
+	// log.Printf("cli %+v! \n", cli)
+	// log.Printf("climap %+v! \n", cliMap)
+	if !reflect.DeepEqual(cliMap, cli) {
+		t.Error("save redis  failed")
+	}
+	_, err = redis.String(red.Do("get", hex.EncodeToString(cli.Address)))
+	if err != nil {
+		t.Error("get address string failed")
+	}
+	// log.Printf("value %+v! \n", values)
 }
 
 // func TestRedis(t *testing.T) {
